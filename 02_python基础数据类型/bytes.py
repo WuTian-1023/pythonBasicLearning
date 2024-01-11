a47d93d33656581a567b932cf754870b0779623f
tianjiming <649401845@qq.com>
2024-01-12 00:52:44 +0800

    bytes.py

--- 02_python基础数据类型/bytes.py
+++ 02_python基础数据类型/bytes.py
@@ -9,4 +9,17 @@
     ANSI => 一套标准 每个字符 16bit , 2byte
 
     2.bytes
+    用户平时看到的数据最终单位都是字节byte
 """
+s = "周杰伦"
+bs1 = s.encode("GBK") #b'\xd6\xdc\xbd\xdc\xc2\xd7'   bytes类型
+bs2 = s.encode("UTF8") # b'\xe5\x91\xa8\xe6\x9d\xb0\xe4\xbc\xa6'
+print(bs1)
+print(bs2)
+
+#怎么把一个gbk的字节转换成utf8的字节
+bs = b'\xd6\xdc\xbd\xdc\xc2\xd7'
+decode = bs.decode("gbk")
+print(decode)
+encode = decode.encode("UTF8")
+print(encode)
